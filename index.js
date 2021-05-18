401c3b67d1c3350da9bd34430f6749894c73ea69
Peter Soriano <timsoriano3@gmail.com>
2021-05-17 21:15:46 -0400

    Node and Express Server Static Files

---
+++ index.js
@@ -0,0 +1,23 @@
+var rect = require('./rectangle');
+
+function solveRect(l, b) {
+    console.log("solving for rectangle with l = " + l + "and b = " + b);
+
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        } else {
+            console.log("The area of the rectangle of dimensions l = " 
+            + l + " and b = " + b + " is " + rectangle.area())
+
+            console.log("The perimeter of the rectangle of dimensions l = " 
+            + l + " and b = " + b + " is " + rectangle.perimeter())
+        }
+    });
+    console.log("this statement is after the call to rect()");
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
