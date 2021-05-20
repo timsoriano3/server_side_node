ed870fe651d9885890904847720a1f5e0b5f25f4
Peter Soriano <timsoriano3@gmail.com>
2021-05-19 21:23:40 -0400

    Assignment 2(complete): REST API with MongoDB

--- conFusionServer/routes/promoRouter.js
+++ conFusionServer/routes/promoRouter.js
@@ -1,35 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Promotions = require('../models/promotions');
 
 const promoRouter = express.Router();
 
 promoRouter.use(bodyParser.json())
 
 promoRouter.route('/')
-    .all((req,res,next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
-    })
     .get((req,res,next) => {
-        res.end('Will send all the promos to you!');
+        Promotions.find({}).then((promotions) => {
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotions);
+        }, (err) => next(err)).catch((err) => next(err));
     })
     .post((req,res,next) => {
-        res.end('Will add the promo: ' + req.body.name + 
-            ' with details: ' + req.body.description)
+        Promotions.create(req.body).then((promotion) =>{
+            console.log('Promotion Created ', promotion);
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        }, (err) => next(err)).catch((err) => next(err));
     })
     .put((req,res,next) => {
         res.statusCode = 403;
         res.end('PUT op not supperted on /promoes');
     })
     .delete((req,res,next) => {
-        res.end('Deleting all the the promos');
+        Promotions.remove({}).then((resp) => {
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+        }, (err) => next(err)).catch((err) => next(err));
     });
 
 promoRouter.route('/:promoId')
     .get((req,res,next) => {
-        res.end('Will send details of the promo: ' + 
-            req.params.promoId + ' to you!');
+        Promotions.findById(req.params.promoId)
+        .then((promotion) =>{
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        }, (err) => next(err)).catch((err) => next(err));
     })
     .post((req,res,next) => {
         res.statusCode = 403;
@@ -37,13 +51,22 @@ promoRouter.route('/:promoId')
             + req.params.promoId);
     })
     .put((req,res,next) => {
-        res.write('Updating the promo: ' + req.params.promoId + '\n');
-        res.end('Will update the promo: ' + 
-            req.body.name + ' with details: ' +
-            req.body.description);
+        Promotions.findByIdAndUpdate(req.params.promoId, {
+            $set: req.body
+        }, { new: true })
+        .then((promotion) =>{
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        }, (err) => next(err)).catch((err) => next(err));
     })
     .delete((req,res,next) => {
-        res.end('Deleting promo: ' + req.params.promoId);
+        Promotions.findByIdAndRemove(req.params.promoId)
+        .then((resp) => {
+            res.StatusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(resp);
+        }, (err) => next(err)).catch((err) => next(err));
     });
-
+    
 module.exports = promoRouter;
