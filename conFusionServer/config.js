efb88c80925fc51e0b5fd39b8950d1ad0854c2c0
Peter Soriano <timsoriano3@gmail.com>
2021-05-29 18:27:48 -0400

    OAuth with Passport and Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-39382-38938-38923',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '304830414591922',
+        clientSecret: 'bc42b30795a3139e22e0bcdd2412b560'
+    }
 }
